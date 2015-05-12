afc12a87be4e2239263924c0be5d2f653ac9feb1
rhoen <rhoen.pa@gmail.com>
2015-05-12 10:50:19 -0400

    add bcrypt gem, add User#password=

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,14 @@ class User < ActiveRecord::Base
   validates :email, :password_digest, presence: true
   validates :password {length: minimum: 6, allow_null: true}
 
-  
+  def User.find_by_credentials(email, password)
+
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+
 end
