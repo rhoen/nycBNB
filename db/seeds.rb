7181b5f532618400ca9222ee3c1332b1a4cb1161
Rhoen Pruesse-Adams <rhoen.pa@gmail.com>
2015-05-13 17:02:31 -0400

    add db seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,46 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+  users = User.create([
+    {email: "ripe@gmail.com", password: "password"},
+    {email: "rhoen@gmail.com", password: "password"},
+    {email: "hello@gmail.com", password: "password"}
+    ])
+
+
+  Listing.create(
+    owner_id: users[0].id,
+    street_address: "598 Broadway",
+    city: "New York",
+    state: "NY",
+    zip: "10003",
+    room_type: "Shared Room",
+    guest_limit: 10,
+    price_per_night: 0.00,
+    description: "AppAcademy office. Free, but must be a student."
+  )
+
+  Listing.create(
+    owner_id: users[1].id,
+    street_address: "208 Mount Hope Place",
+    city: "Bronx",
+    state: "NY",
+    zip: "10457",
+    room_type: "Private Room",
+    guest_limit: 1,
+    price_per_night: 35.00,
+    description: "Beautiful but small room"
+  )
+
+  Listing.create(
+    owner_id: users[2].id,
+    street_address: "100 MacDougal",
+    city: "New York",
+    state: "NY",
+    zip: "10003",
+    room_type: "Apartment",
+    guest_limit: 4,
+    price_per_night: 600.00,
+    description: "Gorgeous apartment with view of Washington Square Park!"
+  )
