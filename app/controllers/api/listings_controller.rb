ee55b39014bfb162f09dc980f70596fd232a925d
Rhoen Pruesse-Adams <rhoen.pa@gmail.com>
2015-05-13 17:56:42 -0400

    fix listingcontroller namespacing

--- app/controllers/api/listings_controller.rb
+++ app/controllers/api/listings_controller.rb
@@ -1,34 +1,36 @@
-class Api::ListingsController < ApiController
-  def create
-    listing = Listing.new listing_params
-    if listing.save
+module Api
+  class ListingsController < ApiController
+    def create
+      listing = Listing.new listing_params
+      if listing.save
+        render json: listing
+      else
+        render json: listing.errors.full_messages
+      end
+
+    end
+
+    def destroy
+      listing = Listing.find(params[:id])
+      listing.destroy
+      render json: true
+    end
+
+    def index
+      listings = Listing.all
+      render json: listings
+    end
+
+    def show
+      listing = Listing.find(params[:id])
       render json: listing
-    else
-      render json: listing.errors.full_messages
+    end
+
+    private
+    def listing_params
+      params.require(:listing).permit(:street_address, :city, :state, :zip,
+      :room_type, :guest_limit, :price_per_night, :description)
     end
 
   end
-
-  def destroy
-    listing = Listing.find(params[:id])
-    listing.destroy
-    render json: true
-  end
-
-  def index
-    listings = Listing.all
-    render json: listings
-  end
-
-  def show
-    listing = Listing.find(params[:id])
-    render json: listing
-  end
-
-  private
-  def listing_params
-    params.require(:listing).permit(:street_address, :city, :state, :zip,
-    :room_type, :guest_limit, :price_per_night, :description)
-  end
-
 end
--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,7 +1,7 @@
 module Api
   class UsersController < ApiController
     def curr_user
-      render json: {user: current_user.email}
+      render json: {email: current_user.email}
     end
   end
 end
