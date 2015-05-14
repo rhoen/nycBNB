56a4b324211f70c02cf8d2a0d24ef49dbbd0a3e4
Rhoen Pruesse-Adams <rhoen.pa@gmail.com>
2015-05-14 11:43:42 -0400

    refactor trip form on listing show page into own template

--- app/assets/javascripts/views/listings/show.js
+++ app/assets/javascripts/views/listings/show.js
@@ -1,4 +1,7 @@
 nycBNB.Views.Listings.Show = Backbone.View.extend({
+  tagName: "section",
+  className: "trip-form",
+  tripForm: JST["trips/form"],
   template: JST["listings/show"],
   initialize: function () {
     this.listenTo(this.model, 'sync', this.render);
@@ -6,6 +9,7 @@ nycBNB.Views.Listings.Show = Backbone.View.extend({
   render: function () {
     // var owner = this.model.owner.fetch();
     this.$el.html(this.template({listing: this.model}));
+    this.$el.append(this.tripForm({listing: this.model}));
     return this;
   }
 })
