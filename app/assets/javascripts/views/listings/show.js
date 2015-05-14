398c0c6b055e4145200e91e944a597cd163668a3
Rhoen Pruesse-Adams <rhoen.pa@gmail.com>
2015-05-14 10:35:18 -0400

    render basic listingView page

--- app/assets/javascripts/views/listings/show.js
+++ app/assets/javascripts/views/listings/show.js
@@ -1,7 +1,10 @@
 nycBNB.Views.Listings.Show = Backbone.View.extend({
   template: JST["listings/show"],
+  initialize: function () {
+    this.listenTo(this.model, 'sync', this.render);
+  },
   render: function () {
-    var owner = this.model.owner.fetch();
+    // var owner = this.model.owner.fetch();
     this.$el.html(this.template({listing: this.model}));
     return this;
   }
