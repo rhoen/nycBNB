56dcdc542844cfde1e747fcde0270a86a1c58f77
Rhoen Pruesse-Adams <rhoen.pa@gmail.com>
2015-05-14 12:03:59 -0400

    add serialize json sourcecode, update listing form

--- app/assets/javascripts/views/listings/form.js
+++ app/assets/javascripts/views/listings/form.js
@@ -3,6 +3,20 @@ nycBNB.Views.Listings.Form = Backbone.View.extend({
   initialize: function () {
     this.listenTo(this.model, 'sync', this.render);
   },
+  events: {
+    "click button.submit" : "createListing"
+  },
+  createListing: function (event) {
+    event.preventDefault();
+    var formData =
+      $(event.currentTarget.parentElement).serializeJSON();
+    this.model.save(formData, {
+      success: function () {
+        Backbone.history.navigate("#listing/" + this.model.id,
+          {trigger: true});
+      }.bind(this)
+    });
+  },
   render: function () {
     this.$el.html(this.template({
       listing: this.model
