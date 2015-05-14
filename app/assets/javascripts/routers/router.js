ce727c21ac04e55675e4b7e27ad6ef8d183d23e5
Rhoen Pruesse-Adams <rhoen.pa@gmail.com>
2015-05-14 10:12:08 -0400

    debug listing index view

--- app/assets/javascripts/routers/router.js
+++ app/assets/javascripts/routers/router.js
@@ -11,23 +11,34 @@ nycBNB.Routers.Router = Backbone.Router.extend({
   initialize: function (options) {
     this.$rootEl = options.$rootEl;
     this.currUser = options.currUser;
+    this.listings = new nycBNB.Collections.Listings();
   },
   listingsIndex: function () {
-    this.listings = new nycBNB.Collections.Listings();
     this.listings.fetch();
     var indexView = new nycBNB.Views.Listings.Index({
       collection: this.listings
     });
 
+    this.$rootEl.html(indexView.render().$el);
   },
   newListing: function () {},
-  listingShow: function () {
-    var listing = new nycBNB.Views.Listings.Show();
+  listingShow: function (id) {
+    var listing = this.listings.getOrFetch(id);
+    var showView = new nycBNB.Views.Listings.Show({
+      model: listing
+    });
+
+    this.$rootEl.html(showView.render().$el);
   },
   editListing: function () {},
 
   landingPage: function () {
-    this.$rootEl.html("you've landed");    
+    this.$rootEl.html("you've landed");
+    var index = new nycBNB.Views.Listings.Index({
+      collection: this.listings
+    });
+    this.listings.fetch();
+    this.$rootEl.append(index.render().$el)
   },
 
   // logout: function (event) {
--- app/assets/javascripts/views/listings/index.js
+++ app/assets/javascripts/views/listings/index.js
@@ -1,7 +1,12 @@
 nycBNB.Views.Listings.Index = Backbone.View.extend({
   tagName: "ul",
   template: JST["listings/index"],
+  initialize: function () {
+    this.listenTo(this.collection, 'sync', this.render);
+  },
   render: function () {
+    debugger
+    console.log('render index');
     this.$el.html(this.template({listings: this.collection}));
     return this;
   }
