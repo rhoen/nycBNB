be419b7d7111a0977499b1aa631a93cdd8ce8391
Rhoen Pruesse-Adams <rhoen.pa@gmail.com>
2015-05-14 13:58:02 -0400

    add activate method to listing class

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,6 +1,6 @@
 class Listing < ActiveRecord::Base
-  validates :owner_id, :street_address, :city, :state, :zip, :room_type,
-    :guest_limit, :price_per_night, presence: true
+  validates :owner_id, :city, :zip, :room_type,
+    :guest_limit, presence: true
 
   belongs_to(
     :owner,
@@ -8,6 +8,22 @@ class Listing < ActiveRecord::Base
     foreign_key: :owner_id,
     inverse_of: :listings
   )
+
+  def activate
+    if self.all_attributes_present
+      self.active = true
+    else
+      false
+    end
+  end
+
+  def all_attributes_present
+    if self.attributes.values.include? nil
+      false
+    else
+      true
+    end
+  end
 
 
 end
