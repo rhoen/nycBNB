1c1cc2987983dc7e68ebe16a98061e59c5e58df7
Rhoen Pruesse-Adams <rhoen.pa@gmail.com>
2015-05-14 09:39:19 -0400

    add title attribute to listings

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150513191546) do
+ActiveRecord::Schema.define(version: 20150514133744) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20150513191546) do
     t.string   "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "title"
   end
 
   add_index "listings", ["city"], name: "index_listings_on_city", using: :btree
