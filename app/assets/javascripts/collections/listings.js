8715005efc9c2b5f7994d2345c61438070e8e578
Rhoen Pruesse-Adams <rhoen.pa@gmail.com>
2015-05-14 09:33:49 -0400

    add getOrFetch to listings collection

--- app/assets/javascripts/collections/listings.js
+++ app/assets/javascripts/collections/listings.js
@@ -1,4 +1,15 @@
 nycBNB.Collections.Listings = Backbone.Collection.extend({
   url: "/api/listings",
-  model: nycBNB.Models.Listing
+  model: nycBNB.Models.Listing,
+  getOrFetch: function(id) {
+    var listing = this.get(id);
+    if (listing) {
+      listing.fetch();
+    } else {
+      listing = new nycBNB.Models.Listing({id: id});
+      listing.fetch();
+    }
+
+    return listing;
+  }
 })
