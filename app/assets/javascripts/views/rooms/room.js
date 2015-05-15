892971bd7c584ca43904fdfdc41d3d4a1110e3e7
Rhoen Pruesse-Adams <rhoen.pa@gmail.com>
2015-05-15 10:33:06 -0400

    add toggle status button to rooms

--- app/assets/javascripts/views/rooms/room.js
+++ app/assets/javascripts/views/rooms/room.js
@@ -3,6 +3,19 @@ nycBNB.Views.Room = Backbone.CompositeView.extend({
   initialize: function () {
     this.listenTo(this.model, 'sync', this.render);
   },
+  events: {
+    "click button.status" : "toggleStatus"
+  },
+  toggleStatus: function (event) {
+    event.preventDefault();
+    if ($(event.currentTarget).hasClass("inactive")) {
+      $(event.currentTarget).removeClass("inactive");
+      $(event.currentTarget).addClass("active");
+    } else {
+      $(event.currentTarget).removeClass("active");
+      $(event.currentTarget).addClass("inactive");
+    }
+  },
   render: function () {
     console.log("render individual Room view");
     this.$el.html(this.template({room: this.model}));
