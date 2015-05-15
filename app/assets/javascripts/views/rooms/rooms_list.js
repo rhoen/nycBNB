6da2668bb1183659192deca6daf839b32a1d6658
Rhoen Pruesse-Adams <rhoen.pa@gmail.com>
2015-05-15 14:03:52 -0400

    add broken functionality to hide room container when collection empty

--- app/assets/javascripts/views/rooms/rooms_list.js
+++ app/assets/javascripts/views/rooms/rooms_list.js
@@ -1,12 +1,18 @@
 nycBNB.Views.RoomsList = Backbone.CompositeView.extend({
   template: JST["rooms/rooms_list"],
   tagName: "section",
+  id: "rooms-list-view-container",
   initialize: function (options) {
     this.title = options.title;
     this.listenTo(this.collection, 'add', this.addRoom);
+    this.listenTo(this.collection, "change", this.checkHide);
     this.status = options.status;
     this.collection.each(this.addRoom.bind(this));
-    this.className = this.status;
+  },
+  checkHide: function () {
+    if (this.collection.length === 0) {
+      $('rooms-list-view-container').addClass("hidden");
+    }
   },
   render: function () {
     console.log("render rooms list view");
@@ -18,6 +24,8 @@ nycBNB.Views.RoomsList = Backbone.CompositeView.extend({
 
       this.$el.html(content);
       this.attachSubviews();
+    } else {
+
     }
 
     return this;
