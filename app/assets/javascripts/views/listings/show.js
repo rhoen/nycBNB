5da9261f42e150834d4e4a4def0242541f7c0284
Rhoen Pruesse-Adams <rhoen.pa@gmail.com>
2015-05-16 15:50:23 -0400

    add owner create, fetch, insert on listing show view

--- app/assets/javascripts/views/listings/show.js
+++ app/assets/javascripts/views/listings/show.js
@@ -4,12 +4,16 @@ nycBNB.Views.Listings.Show = Backbone.View.extend({
   tripForm: JST["trips/form"],
   template: JST["listings/show"],
   initialize: function () {
+    this.owner = new nycBNB.Models.User({id: this.model.owner_id});
+    this.owner.fetch();
     this.listenTo(this.model, 'sync', this.render);
+    this.listenTo(this.owner, 'sync', this.render);
   },
   render: function () {
-    // var owner = this.model.owner.fetch();
-
-    this.$el.html(this.template({listing: this.model}));
+    this.$el.html(this.template({
+      listing: this.model,
+      owner: this.owner
+    }));
     this.$el.append(this.tripForm({listing: this.model}));
     return this;
   }
