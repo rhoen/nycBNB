50ab8d59c68e7aa8abed6fec7ae32623cb48b732
Rhoen Pruesse-Adams <rhoen.pa@gmail.com>
2015-05-15 20:08:53 -0400

    style list your space link

--- app/assets/javascripts/views/dashboard/dashboard.js
+++ app/assets/javascripts/views/dashboard/dashboard.js
@@ -1,5 +1,13 @@
 nycBNB.Views.Dashboard.Dashboard = Backbone.CompositeView.extend({
   dashNav: JST["shared/dashboard_nav"],
+  events: {
+    "click button":"createListing"
+  },
+  createListing: function (event) {
+    debugger
+    event.preventDefault();
+    Backbone.history.navigate("listings/new", {trigger: true});
+  },
   render: function () {
     this.$el.html(this.dashNav());
     this.$el.append("<div id='dash-content'></div>");
