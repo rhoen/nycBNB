d25386492d527d2899818d4a1419497e55b9e1b7
Rhoen Pruesse-Adams <rhoen.pa@gmail.com>
2015-05-16 13:39:56 -0400

    remove event click button on dash view

--- app/assets/javascripts/views/dashboard/dashboard.js
+++ app/assets/javascripts/views/dashboard/dashboard.js
@@ -1,13 +1,5 @@
 nycBNB.Views.Dashboard.Dashboard = Backbone.CompositeView.extend({
   dashNav: JST["shared/dashboard_nav"],
-  events: {
-    "click button":"createListing"
-  },
-  createListing: function (event) {
-    debugger
-    event.preventDefault();
-    Backbone.history.navigate("listings/new", {trigger: true});
-  },
   render: function () {
     this.$el.html(this.dashNav());
     this.$el.append("<div id='dash-content'></div>");
