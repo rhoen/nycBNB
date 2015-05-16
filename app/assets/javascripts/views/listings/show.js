feff4a39f6d7d1cab63d320df98faf88bb8bc9b2
Rhoen Pruesse-Adams <rhoen.pa@gmail.com>
2015-05-16 16:46:09 -0400

    add placeholder function for addOwnerView on listing show

--- app/assets/javascripts/views/listings/show.js
+++ app/assets/javascripts/views/listings/show.js
@@ -1,10 +1,11 @@
-nycBNB.Views.Listings.Show = Backbone.View.extend({
+nycBNB.Views.Listings.Show = Backbone.CompositeView.extend({
   tagName: "section",
   className: "trip-form",
   tripForm: JST["trips/form"],
   template: JST["listings/show"],
   initialize: function () {
-    this.owner = new nycBNB.Models.User({id: this.model.owner_id});
+    debugger
+    this.owner = new nycBNB.Models.User({id: this.model.get("owner_id")});
     this.owner.fetch();
     this.listenTo(this.model, 'sync', this.render);
     this.listenTo(this.owner, 'sync', this.render);
