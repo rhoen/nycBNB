f3b024ec983b4445064aff49b90c7acab4a8cb41
Rhoen Pruesse-Adams <rhoen.pa@gmail.com>
2015-05-16 20:39:20 -0400

    add listeners for submit button on listing form

--- app/assets/javascripts/views/listings/form.js
+++ app/assets/javascripts/views/listings/form.js
@@ -2,11 +2,39 @@ nycBNB.Views.Listings.Form = Backbone.View.extend({
   template: JST["listings/form"],
   initialize: function () {
     this.listenTo(this.model, 'sync', this.render);
+    this.listenTo($('.home-type'), 'change', this.validateSubmit)
+    this.listenTo($('.room-type'), 'change', this.validateSubmit)
+    this.listenTo($('#city'), 'change', this.validateSubmit)
+    this.listenTo($('#price'), 'change', this.validateSubmit)
+    this.listenTo($('#title'), 'change', this.validateSubmit)
   },
   events: {
     "click button.submit" : "createListing",
     "click button.toggle-address" : "toggleAddress"
   },
+  validateSubmit: function () {
+    if (!this.home-type) {
+      this.homeType = $('.home-type input');
+      this.roomType = $('.room-type');
+      this.price = $('#price');
+      this.title = $('#title');
+    }
+    if (
+      this.homeType.filter("checked") &&
+      this.roomType.filter("checked") &&
+      this.price.val() !== "" &&
+      this.title.val() !== ""
+    ) {
+      $("button.submit").removeClass(".disabled")
+      $("button.submit").addEventListener("click", this.createListing)
+    } else {
+      $("button.submit").addClass(".disabled")
+      $("button.submit").removeEventListener("click", this.createListing)
+    }
+
+  },
+  ensureEnabled: function () {},
+  ensureDisabled: function() {},
   toggleAddress: function(event) {
     event.preventDefault();
     $details = $('section.address-details');
