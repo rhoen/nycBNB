3ca1e223726c4cfe7978cdae619839ee86553ade
Rhoen Pruesse-Adams <rhoen.pa@gmail.com>
2015-05-17 16:35:13 -0400

    migrate paperclip avatar on users, add validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
   validates :email, :password_digest, presence: true
   validates :password, length: {minimum: 6, allow_nil: true}
   validates :email, uniqueness: true
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
   has_many :sessions, dependent: :destroy
   has_many(
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150515135759) do
+ActiveRecord::Schema.define(version: 20150517203257) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,10 +52,14 @@ ActiveRecord::Schema.define(version: 20150515135759) do
   add_index "sessions", ["session_token"], name: "index_sessions_on_session_token", unique: true, using: :btree
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",           null: false
-    t.string   "password_digest", null: false
+    t.string   "email",               null: false
+    t.string   "password_digest",     null: false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
