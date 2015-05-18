40f5d16ac52026cffb81d9f94beae44df6d765f3
Rhoen Pruesse-Adams <rhoen.pa@gmail.com>
2015-05-18 10:38:30 -0400

    update dashboard jbuilder

--- app/assets/javascripts/views/dashboard/dashboard.js
+++ app/assets/javascripts/views/dashboard/dashboard.js
@@ -2,6 +2,7 @@ nycBNB.Views.Dashboard.Dashboard = Backbone.CompositeView.extend({
   template: JST["shared/dashboard"],
   initialize: function(options) {
     this.currUser = options.currUser;
+    this.listenTo(this.currUser, "sync", this.render);
   },
   render: function () {
     this.$el.html(this.template({
--- app/views/users/curr_user.json.jbuilder
+++ app/views/users/curr_user.json.jbuilder
@@ -1,2 +1,6 @@
-json.email = current_user.email
-json.image_url asset_path(current_user.avatar.url(:thumb))
+json.email current_user.email
+unless current_user.avatar.nil?
+  json.image_url asset_path(current_user.avatar.url(:thumb))
+else
+  json.image_url asset_path("app/assets/images/image_missing.png")
+end
