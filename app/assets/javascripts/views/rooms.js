b7057228f32e0b86a1b26829d1cdec5fe2be56d9
Rhoen Pruesse-Adams <rhoen.pa@gmail.com>
2015-05-19 18:24:14 -0400

    add and remove selected class on clicks in rooms view

--- app/assets/javascripts/views/rooms.js
+++ app/assets/javascripts/views/rooms.js
@@ -12,9 +12,12 @@ nycBNB.Views.Rooms = Backbone.CompositeView.extend({
   detailView: function (event) {
     console.log("detail view method");
     if (this._photoEditView) {
+      var id = this._photoEditView.model.id
+      $(".room").data({id: id}).removeClass("selected");
       this.removeSubview("#detail-view", this._photoEditView);
     }
     $target = $(event.target);
+    $target.addClass("selected");
     var room = this.collection.getOrFetch($target.data('id'));
     this._photoEditView = new nycBNB.Views.Listings.PhotoEdit({
       model: room
