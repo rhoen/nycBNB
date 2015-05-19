362d38a546abde9a679d83efce7ef4afe7fd2468
Rhoen Pruesse-Adams <rhoen.pa@gmail.com>
2015-05-19 19:36:03 -0400

    add savePhoto method to photoedit view

--- app/assets/javascripts/views/listings/photo_edit.js
+++ app/assets/javascripts/views/listings/photo_edit.js
@@ -2,7 +2,26 @@ nycBNB.Views.Listings.PhotoEdit = Backbone.CompositeView.extend({
   // className: "photo-edit",
   template: JST["listings/photo_edit"],
   initialize: function () {
-    // this.listenTo(this.model, 'sync', this.render)
+    this.listenTo(this.model, 'sync', this.render)
+  },
+  events: {
+    "click .add-photo-button": "savePhoto"
+  },
+  savePhoto: function(event) {
+    event.preventDefault();
+    var file = $.find(".add-photo input")[0].files[0];
+    var reader = new FileReader();
+    reader.onloadend = function () {
+      var result = reader.result;
+      var photo = new nycBNB.Models.ListingPhoto()
+      photo.save({
+        listing_id: this.model.id,
+        photo: result
+      })
+    }.bind(this);
+    if (file) {
+      reader.readAsDataURL(file);
+    }
   },
   render: function () {
     console.log("photo edit view render");
