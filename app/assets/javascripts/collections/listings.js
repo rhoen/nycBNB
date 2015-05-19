c8487587a7c3a00173cc17e2f84c65c92745ddb8
Rhoen Pruesse-Adams <rhoen.pa@gmail.com>
2015-05-19 15:21:49 -0400

    add parse method to map each listing under listing header

--- app/assets/javascripts/collections/listings.js
+++ app/assets/javascripts/collections/listings.js
@@ -1,6 +1,11 @@
 nycBNB.Collections.Listings = Backbone.Collection.extend({
   url: "/api/listings",
   model: nycBNB.Models.Listing,
+  parse: function(payload) {
+    return _.map(payload, function(list){
+      return {listing: list};
+    });
+  },
   getOrFetch: function(id) {
     var listing = this.get(id);
     if (listing) {
