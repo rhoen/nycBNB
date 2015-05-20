cac84232d3a96087bb04968f9b77e21cdfa0fb65
Rhoen Pruesse-Adams <rhoen.pa@gmail.com>
2015-05-20 12:19:05 -0400

    add search form

--- app/assets/javascripts/views/maps/main.js
+++ app/assets/javascripts/views/maps/main.js
@@ -4,15 +4,20 @@ nycBNB.Views.Maps.Main = Backbone.View.extend({
   className: "clearfix",
   template: JST["maps/main"],
   initialize: function () {
-    this.mapView = new nycBNB.Views.Maps.Map(); //pass collection?
+    this.collection = new nycBNB.Collections.listings();
+    this.mapView = new nycBNB.Views.Maps.Map({
+      collection: this.collection
+    }); //pass collection?
     // this.addSubview("#map-canvas", mapView); don't do this?
-    // this.searchFilterView = new nycBNB.Views.Maps.searchFilter();
+    this.searchView = new nycBNB.Views.Maps.Search({
+      collection: this.collection
+    });
   },
   render: function () {
 
     this.$el.html(this.template());
     this.$el.append(this.mapView.$el);
-    // $("#map-search-filter").html(this.searchFilterView.render());
+    // $("#map-search-filter").html(this.searchView.render());
     this.mapView.initMap();
 
     return this;
---
+++ app/assets/javascripts/views/maps/serach.js
@@ -0,0 +1,10 @@
+nycBNB.Views.Maps.Search = Backbone.View.extend({
+  template: JST[""],
+  form: JST["maps/search_form"],
+  className: ""
+  tagName: "section"
+
+  render: function () {
+
+  },
+})
