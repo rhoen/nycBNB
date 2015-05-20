2c1b089298e0852a3adf6f27b4c40ac67353de6b
Rhoen Pruesse-Adams <rhoen.pa@gmail.com>
2015-05-19 20:53:36 -0400

    add default room_missing, complete photo jbuilder

--- app/models/listing_photo.rb
+++ app/models/listing_photo.rb
@@ -1,6 +1,6 @@
 class ListingPhoto < ActiveRecord::Base
   validates :listing_id, presence: true
-  has_attached_file :photo
+  has_attached_file :photo, default_url: "room_missing.png", styles: { thumb: ["32x32#", :jpg], large: ["400x300#", :jpg] }
   validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
 
   belongs_to :listing
--- app/views/listings/show.json.jbuilder
+++ app/views/listings/show.json.jbuilder
@@ -1,2 +1,5 @@
 json.listing @listing
-json.photos @listing.listing_photos
+json.array! @listing.listing_photos do |photo|
+  json.thumb_url asset_path(photo.url(:thumb))
+  json.large_url asset_path(photo.url(:large))
+end
