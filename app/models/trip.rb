0d986ebe902b25a637f7d222a1c6556fe861285b
Rhoen Pruesse-Adams <rhoen.pa@gmail.com>
2015-06-05 17:52:55 -0400

    add methods to trip model

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -10,41 +10,58 @@ class Trip < ActiveRecord::Base
   validates :listing_id, :traveler_id, :start_date, :end_date, presence: true
   validate  :listing_is_available
   validate :start_before_end
-  # validate :does_not_overlap_approved_request
+  validate :does_not_overlap_approved_trip
   after_initialize :assign_pending_status
 
-  def listing_is_available
-
-    query = <<-SQL
-      SELECT
-        DISTINCT l.*
-      FROM
-        listings l
-      INNER JOIN
-        trips t ON t.listing_id = l.id
-      WHERE
-         (t.start_date > ? AND t.end_date < ?)
-    SQL
-
-    Listing.find_by_sql [query, t.start_date, t.end_date]
+  def overlapping_trips
     Trip
       .where("(:id IS NULL) OR (id != :id)", id: self.id)
-      .where(id: id)
-      .where(<<-SQL, start_date: start_date, end_date: end_date)
+      .where(listing_id: self.listing_id)
+      .where(<<-SQL, start_date: self.start_date, end_date: self.end_date)
        NOT( (start_date > :end_date) OR (end_date < :start_date) )
-SQL
+      SQL
+  end
 
+  def approve!
+    raise "not pending" unless self.status == "PENDING"
+    transaction do
+      self.status = "APPROVED"
+      self.save!
+
+      # reject all other overlapping pending trips
+      self.overlapping_pending_trips.update_all(status: 'DENIED')
+    end
+  end
+  def approved?
+    self.status == "APPROVED"
+  end
+  def denied?
+    self.status == "DENIED"
+  end
+  def deny!
+    self.status = "DENIED"
+    self.save!
+  end
+  def pending?
+    self.status == "PENDING"
   end
 
   private
-  # def trip_params
-  #   params.require(:trip).permit(:listing_id, :traveler_id, :start_date, :end_date)
-  # end
+  def overlapping_pending_trips
+    overlapping_trips.where("status = 'PENDING'")
+  end
    def assign_pending_status
      self.status ||= "PENDING"
    end
    def start_before_end
     return unless start_date && end_date
-    errors[:start_date] << "must come before end date" if start_date >= end_date
+    if start_date >= end_date
+      errors.add(:start_date, "must come before end date")
+    end
+  end
+  def does_not_overlap_approved_trip
+    unless self.overlapping_trips.where(status: "APPROVED").empty?
+      errors.add(:base, "Trip conflicts with existing approved trip")
+    end
   end
 end
