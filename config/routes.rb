b042e5a197b79c09e29e213b4dc75c2d70d3229d
Rhoen Pruesse-Adams <rhoen.pa@gmail.com>
2015-06-05 21:17:24 -0400

    add routes for trips

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
     resources :listings, only: [:create, :destroy, :index, :show, :update]
     resources :users, only: [:show, :update]
     resources :listing_photos, only: [:create, :destroy, :update]
+    resources :trips, only: [:show, :update, :index, :create, :destroy]
     get "listings/:id/listing_photos" => "listings#listing_photos"
   end
 
