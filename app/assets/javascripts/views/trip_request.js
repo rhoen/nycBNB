47fabcba644d240487a7aa62490f8b1c340c7af3
Rhoen Pruesse-Adams <rhoen.pa@gmail.com>
2015-06-10 17:48:42 -0400

    add approve buttons and action to requests view

--- app/assets/javascripts/views/trip_request.js
+++ app/assets/javascripts/views/trip_request.js
@@ -4,6 +4,29 @@ nycBNB.Views.TripRequest = Backbone.CompositeView.extend({
   initialize: function(options) {
     this.traveler = options.traveler;
     this.listenTo(this.traveler, 'sync', this.render);
+    this.listenTo(this.model, 'sync', this.render);
+  },
+  events: {
+    'click .approve-request' : "approveRequest",
+    'click .deny-request' : "denyRequest"
+  },
+  approveRequest: function(event) {
+    this.model.save({
+      status: "APPROVED"
+    }, {
+      success: function () {
+        console.log("request approved!");
+      }
+    })
+  },
+  denyRequest: function(event) {
+    this.model.save({
+      status: "DENIED"
+    }, {
+      success: function () {
+        console.log('request denied');
+      }
+    })
   },
   render: function () {
     console.log("render trip request");
