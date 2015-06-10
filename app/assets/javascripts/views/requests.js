7d4f2bdfe1135ebe16c06c6c25d0d33455e7521b
Rhoen Pruesse-Adams <rhoen.pa@gmail.com>
2015-06-09 22:31:06 -0400

    add trips to listing index jbuilder

--- app/assets/javascripts/views/requests.js
+++ app/assets/javascripts/views/requests.js
@@ -2,8 +2,10 @@ nycBNB.Views.Requests = Backbone.CompositeView.extend({
   template: JST["trips/requests"],
   initialize: function () {
     //collection is of current user's listings
+    this.listenTo(this.collection, 'sync', this.render)
   },
   render: function () {
+    console.log("requests render");
     this.$el.html(this.template({
       listings: this.collection
     }));
