1d8ee2051f5567a5f0eb4ce9b0d21949dcf7ef5b
Rhoen Pruesse-Adams <rhoen.pa@gmail.com>
2015-06-09 23:19:03 -0400

    add request subviews

---
+++ app/assets/javascripts/views/listing_requests.js
@@ -0,0 +1,7 @@
+nycBNB.Views.ListingRequests = Backbone.CompositeView.extend({
+  template: JST["trips/request"],
+  render: function () {
+
+    return this;
+  }
+})
--- app/assets/javascripts/views/requests.js
+++ app/assets/javascripts/views/requests.js
@@ -1,15 +1,21 @@
 nycBNB.Views.Requests = Backbone.CompositeView.extend({
   template: JST["trips/requests"],
   id: "requests-are-made",
+  addListingSubviews: function () {
+    this.collection.forEach(function(listing){
+      var listingSubview = new nycBNB.Views.ListingRequests({model: listing});
+      this.addSubview("#listings", listingSubview);
+    })
+  },
   initialize: function () {
     //collection is of current user's listings
-    this.listenTo(this.collection, 'sync', this.render)
+    // this.listenTo(this.collection, 'sync', this.render)
   },
   render: function () {
-    console.log("requests render");
     this.$el.html(this.template({
       listings: this.collection
     }));
+    this.addListingSubviews();
     return this;
   }
 });
